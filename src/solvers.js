bdcf0bb9c49608cbd47f0bdfffd44811952c48d9
wongal5 <alberthwong2@gmail.com>
2017-12-07 11:43:43 -0800

    first draft of countNRooksSolutions

--- src/solvers.js
+++ src/solvers.js
@@ -64,49 +64,51 @@ window.findNRooksSolution = function(n) {
 
 // start by toggling a random index? then check if the arrays are equal, if not, count++
 window.countNRooksSolutions = function(n) {
-  var newBoard = new Board( { 'n': n } );
-  var solution = newBoard.rows();
-  newBoard.togglePiece(0, 0);
+  var board = new Board({ 'n': n});
+  var counter = n;
   
-  var flag = false;
-  var fillNextValidSpace = function() {
-    flag = false;
-    
-    for (var a = 0; a < n; a++) {
-      for (var b = 0; b < n; b++) {
-        if (solution[a][b] === 0) {
-          // 
-          if (!newBoard.hasRowConflictAtUsed(a) && !newBoard.hasColConflictAtUsed(b)) {
-            newBoard.togglePiece(a, b);
-            flag = true;
-            a = n;
-            b = n;
-          } else {
-            solution[a][b] = null;
+  var compileMoves = function(curBoard) {
+    var boardArray = curBoard.rows();
+    var optionsArray = [];
+    for (var a = 0; a < boardArray.length; a++) {
+      for (var b = 0; b < boardArray.length; b++) {
+        if (boardArray[a][b] === 0) {
+          if (!curBoard.hasRowConflictAtUsed(a) && !curBoard.hasColConflictAtUsed(b)) {
+            optionsArray.push([a, b]);
           }
         }
       } 
     }
-    
-    if (flag) {
-      return fillNextValidSpace();
-    }
-    return null;
+    return optionsArray;
   };
-    
-  fillNextValidSpace();
   
-  // change nulls back to 0
-  for (var c = 0; c < solution.length; c++) {
-    for (var d = 0; d < solution[c].length; d++) {
-      if (_.isNull(solution[c][d])) {
-        solution[c][d] = 0;
-      }
+  var populatePossibleBoards = function (currentBoard, counter, validBoardObject, moveArray) {
+    if (moveArray) {
+      currentBoard.togglePiece(moveArray[0], moveArray[1]);
     }
-  }
-
-  console.log('Number of solutions for ' + n + ' rooks:', solution);
-  return solution;
+    if (counter === 0) {
+      validBoardObject[JSON.stringify(currentBoard.rows())] = 1;
+      currentBoard.togglePiece(moveArray[0], moveArray[1]);
+      return validBoardObject;
+    }
+    counter--;
+    
+    var optionsArray = compileMoves(currentBoard);
+    
+    for (var i = 0; i < optionsArray.length; i++) {
+      _.extend(validBoardObject, populatePossibleBoards(currentBoard, counter, validBoardObject, optionsArray[i]));
+    }
+    
+    if (moveArray) {
+      currentBoard.togglePiece(moveArray[0], moveArray[1]);
+    }
+    return validBoardObject;
+  };
+  
+  var solution = populatePossibleBoards(board, counter, {});
+  var solutionCount = Object.keys(solution).length;
+  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
+  return solutionCount;
 };
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
