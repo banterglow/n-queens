f43dcdbab893433dd59a2b5c1a2d1262ea1fe900
wongal5 <alberthwong2@gmail.com>
2017-12-06 20:52:12 -0800

    passed findNRooksSolution test

--- src/solvers.js
+++ src/solvers.js
@@ -16,42 +16,42 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = new Board( { n: n } );
-  var boardArrays = solution.rows();
-  solution.togglePiece(0, 0);
+  var newBoard = new Board( { 'n': n } );
+  var solution = newBoard.rows();
+  newBoard.togglePiece(0, 0);
   
   var flag = false;
-  var outerRecursion = function() {
+  var fillNextValidSpace = function() {
     flag = false;
     
     for (var a = 0; a < n; a++) {
       for (var b = 0; b < n; b++) {
-        if (boardArrays[a][b] === 0) {
-          if (!solution.hasRowConflictAtUsed(a) && !solution.hasColConflictAtUsed(b)) {
-            solution.togglePiece(a, b);
+        if (solution[a][b] === 0) {
+          if (!newBoard.hasRowConflictAtUsed(a) && !newBoard.hasColConflictAtUsed(b)) {
+            newBoard.togglePiece(a, b);
             flag = true;
             a = n;
             b = n;
           } else {
-            boardArrays[a][b] = null;
+            solution[a][b] = null;
           }
         }
       } 
     }
     
     if (flag) {
-      return outerRecursion();
+      return fillNextValidSpace();
     }
     return null;
   };
     
-  outerRecursion();
+  fillNextValidSpace();
   
   // change nulls back to 0
-  for (var c = 0; c < boardArrays.length; c++) {
-    for (var d = 0; d < boardArrays[c].length; d++) {
-      if (_.isNull(boardArrays[c][d])) {
-        boardArrays[c][d] = 0;
+  for (var c = 0; c < solution.length; c++) {
+    for (var d = 0; d < solution[c].length; d++) {
+      if (_.isNull(solution[c][d])) {
+        solution[c][d] = 0;
       }
     }
   }
